d976be5a8cc6dd5a2a367fd23455a8aa68e9fbc3
Damonlh <myorderrecipt@gmail.com>
2023-02-16 19:56:18 +0000

    Commit

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +12,38 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        
+        int max = 0;
+        char maxChar = ' ';
+
+        HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+
+        char[] arr = str.toCharArray();
+
+        for (char ch : arr)
+        {
+            if (charCount.containsKey(ch)) 
+            {
+                charCount.put(ch, charCount.get(ch) + 1);   
+            }
+            else
+            {
+                charCount.put(ch, 1);
+            }
+        }
+
+        for (Map.Entry entry : charCount.entrySet())
+        {
+            if (max < (int) entry.getValue())
+            {
+                max = (int) entry.getValue();
+                maxChar = (char) entry.getKey();
+            }
+
+        }
+       
+        
+
+        return maxChar;
     }
 }
